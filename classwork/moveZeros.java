fcfac9c9a7dfb69459b2bf9701d8320faa4615c5
Raushan kumar <raushank7460@gmail.com>
2025-09-07 14:14:28 +0530

    update code

--- classwork/moveZeros.java
+++ classwork/moveZeros.java
@@ -1,8 +1,29 @@
 package classwork;
 
+import java.util.Arrays;
+
 public class moveZeros {
     public static void main(String[] args) {
-        System.out.println("welcome Git");
+        int[] nums = {0, 1, 0, 3, 12};
         
+        moveZeroes(nums);
+        
+        System.out.println(Arrays.toString(nums));  // Print the modified array
+    }
+    
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+        
+        // Move non-zero elements to the front
+        for (int ele : nums) {
+            if (ele != 0) {
+                nums[index++] = ele;
+            }
+        }
+        
+        // Fill remaining elements with zeros
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
     }
 }
--- classwork/reverseString.java
+++ classwork/reverseString.java
@@ -1,7 +1,20 @@
 package classwork;
 
+import java.util.Arrays;
+
 public class reverseString {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        
+        int left = 0;
+        int right = s.length - 1;
+        
+        while (left < right) {
+            char temp = s[left];
+            s[left++] = s[right];
+            s[right--] = temp;
+        }
+        
+        System.out.println(Arrays.toString(s));  // Output the reversed array
     }
 }
